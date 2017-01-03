6b869fb258705165d7a671fcf233a4a37e9f804b
leechanjun <thejunys@naver.com>
2017-01-03 11:07:53 +0900

    찬준

--- src/test/product/dao/ProductDao.java
+++ src/test/product/dao/ProductDao.java
@@ -1,8 +1,10 @@
 package test.product.dao;
 
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 
 import test.mybatis.SqlMapConfig;
+import test.product.dto.ProductDto;
 
 public class ProductDao {
 	private static ProductDao dao;
@@ -10,12 +12,26 @@ public class ProductDao {
 
 	private ProductDao() {
 	}
-
+	
 	public static ProductDao getInstance() {
 		if (dao == null) {
 			dao = new ProductDao();
 			factory = SqlMapConfig.getSqlSession();
 		}
 		return dao;
+	}
+	
+	public void insert(ProductDto dto){
+		SqlSession session = factory.openSession();
+		boolean insert = false;
+		try {
+			session.insert("product.insert",dto);
+			insert = true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			session.close();
+		}
 	}
 }
