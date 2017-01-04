272f0afefdfdc5f3039c041c1861084922a756bc
USER <USER@USER-PC>
2017-01-04 18:13:17 +0900

    댓글 수정중

--- src/test/evaluation/action/EvaluationInsertAction.java
+++ src/test/evaluation/action/EvaluationInsertAction.java
@@ -1,5 +1,32 @@
 package test.evaluation.action;
 
-public class EvaluationInsertAction {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import test.controller.Action;
+import test.controller.ActionForward;
+import test.evaluation.dao.EvaluationDao;
+import test.evaluation.dto.EvaluationDto;
+
+public class EvaluationInsertAction extends Action{
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) {
+		// 파라미터로 전송되는 글 제목, 글 내용 읽어오기
+				String title = request.getParameter("title");
+				String content = request.getParameter("ir1");
+				
+				// 세션에 저장된 id 정보 읽어오기
+				String id = (String)request.getSession().getAttribute("id");
+				
+				// CafeDto 에 정보 담기
+				EvaluationDto dto = new EvaluationDto();
+				dto.setEvaWriter(id);
+				dto.setEvaTitle(title);
+				dto.setEvaContent(content);
+				
+				// CafeDao 객체를 이용해서 DB에 저장하기
+				EvaluationDao.getInstance().insert(dto);
+				
+				return new ActionForward("/evaluation/list.do", true);
+	}
 }
--- src/test/evaluation/action/EvaluationUpdateformAction.java
+++ src/test/evaluation/action/EvaluationUpdateformAction.java
@@ -1,5 +1,23 @@
 package test.evaluation.action;
 
-public class EvaluationUpdateformAction {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import test.controller.Action;
+import test.controller.ActionForward;
+import test.evaluation.dao.EvaluationDao;
+import test.evaluation.dto.EvaluationDto;
+
+public class EvaluationUpdateformAction extends Action{
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) {
+		// 1. 파라미터로 전달되는 수정할 글 번호 읽어오기
+		int num = Integer.parseInt(request.getParameter("num"));
+		// 2. 수정할 글의 정보 얻어오기
+		EvaluationDto dto = EvaluationDao.getInstance().getData(num);
+		// 3. request 에 dto를 담고
+		request.setAttribute("dto", dto);
+		// 4. 수정할 글의 정보 출력해주기
+		return new ActionForward("/views/cafe/private/updateform.jsp");
+	}
 }
