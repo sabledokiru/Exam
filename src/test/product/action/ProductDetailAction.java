9e438121dfcbe91a5de47abe75fc1a534342cb63
anstn <anstn@DESKTOP-979IKIJ>
2017-01-05 12:50:38 +0900

    ㅊㅊ

--- src/test/product/action/ProductDetailAction.java
+++ src/test/product/action/ProductDetailAction.java
@@ -2,6 +2,7 @@ package test.product.action;
 
 import java.util.List;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -21,6 +22,11 @@ public class ProductDetailAction extends Action{
 			ProductDao.getInstance().increaseViewCount(num);
 			//3. 글번호에 해당되는 글정보를 얻어온다.
 			ProductDto dto=ProductDao.getInstance().getData(num);
+			
+			//쿠키로 자세히보기한 상품저장.
+			Cookie cookie = new Cookie("sname"+num,ProductDao.getInstance().getData(num).getSaveFileName());
+			response.addCookie(cookie);
+			
 			//4. request 에 글정보를 담는다.
 			request.setAttribute("dto", dto);
 			request.setAttribute("num", num);
